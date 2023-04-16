4ffe30d1bc7b9855447df700e63575af7ceaca0c
yarden <hrsink@gmail.com>
2023-04-16 22:21:34 +0300

    B

--- higher-lower-game/main.py
+++ higher-lower-game/main.py
@@ -6,7 +6,7 @@ print(art.logo)
 score = 0 
 game_on = True
 random_A = random.choice(game_data.data)
-random_b = random.choice(game_data.data)
+random_B = random.choice(game_data.data)
 
 def printing_data():
     
